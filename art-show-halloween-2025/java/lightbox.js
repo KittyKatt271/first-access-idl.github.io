f48b5a2e44be59d4e2937f5846d34b55a31fbdcd
KittyKatt271 <katelyn.woodworth@students.first.edu>
2025-10-06 20:48:36 -0500

    demo-gal2

---
+++ art-show-halloween-2025/java/lightbox.js
@@ -0,0 +1,24 @@
+let lightboxContainer = document.getElementById('lightboxContainer');
+let lightbox = document.getElementById('lightbox');
+
+let galleryImages = document.getElementsByClassName('galleryImage');
+
+for (let i = 0; i < galleryImages.length; i++) {
+    let galleryImage = galleryImages[i];
+    galleryImage.onclick = function() {
+        lightboxContainer.classList.add('display');
+        lightbox.src = this.src;
+        lightbox.alt = this.alt;
+    }
+}
+
+let closers = document.getElementsByClassName('closer');
+
+for (let i = 0; i < closers.length; i++) {
+    let closer = closers[i];
+    closer.onclick = function() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+        lightbox.alt = "";
+    }
+}
